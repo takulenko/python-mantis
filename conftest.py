c83e32ccc50f8d1d9da67986d1175c1f7f36f098
takulenko <t.akulenko@quality-lab.ru>
2025-04-06 23:01:02 +0300

    fix (9.25)

--- conftest.py
+++ conftest.py
@@ -28,10 +28,9 @@ def config(request):
 def app(request, config):
     global fixture
     browser = request.config.getoption("--browser")
-    if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, config=config)
-        sleep(3)
-        fixture.session.ensure_login(username=config["webadmin"]["username"], password=config["webadmin"]["password"])
+    fixture = Application(browser=browser, config=config)
+    sleep(3)
+    fixture.session.ensure_login(username=config["webadmin"]["username"], password=config["webadmin"]["password"])
     return fixture
 
 
